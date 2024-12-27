855452aa70bbc7e9fcaeccfaea0da73a1901ef3a
Gustavo Minetoma <gminetoma@gmail.com>
2024-12-27 13:51:49 +0900

    feat: add `.env.development`
    Update server and Docker ports to avoid conflicts.
    Remove other `.env` files to reduce confusion.
    Introduce `NODE_ENV` environment variable.

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,14 +7,16 @@ declare namespace Express {
 
 declare namespace NodeJS {
   export interface ProcessEnv {
-    PORT: string
-    PROJECT_NAME: string
     DOMAIN: string
-    TOKEN_SECRET: string
-    PEPPER_SECRET: string
+    NODE_ENV: 'development' | 'production'
     NODE_MAILER_SERVICE: string
     NODE_MAILER_USER: string
     NODE_MAILER_PASS: string
     NODE_MAILER_SENDER: string
+    PEPPER_SECRET: string
+    PORT: string
+    PROJECT_NAME: string
+    TOKEN_SECRET: string
+    DATABASE_URL: string
   }
 }
