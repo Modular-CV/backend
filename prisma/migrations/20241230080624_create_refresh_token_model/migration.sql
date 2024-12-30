0f5fee3c727b807bd5b85d8525ad1fb0253860e3
Gustavo Minetoma <gminetoma@gmail.com>
2024-12-30 20:27:36 +0900

    feat: add RefreshToken model
    - Change `id` from `Int` to `String` using `cuid()` to generate.

---
+++ prisma/migrations/20241230080624_create_refresh_token_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "tokenHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_accountId_key" ON "RefreshToken"("accountId");
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
