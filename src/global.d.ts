cadb0432d466f99ff1a6e89cc941eb1be20080e6
Gustavo Minetoma <gminetoma@gmail.com>
2024-12-30 20:30:42 +0900

    feat: add access/refresh token keys
    - Improve the declaration file.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,50 +1,64 @@
 import * as core from 'express-serve-static-core'
 import { RequestHandler as ExpressRequestHandler } from 'express'
-import JwtPayload from 'jsonwebtoken'
 import { ZodIssue } from 'zod'
 import { ErrorCodes } from './types'
+import { Account } from '@prisma/client'
+import { JwtPayload } from 'jsonwebtoken'
+import { Routes } from './routes'
+import TestAgent from 'supertest/lib/agent'
+import { Test } from 'supertest'
 
 declare global {
+  type RequestAccount = Pick<Account, 'id' | 'email'>
+
+  type CustomJwtPayload = { account: RequestAccount } & JwtPayload
+
+  type SuperRequest = Omit<TestAgent, 'post' | 'get'> &
+    Record<'post' | 'get', (route: Routes) => Test>
+
+  type ResponseBody<ErrorCode = keyof typeof ErrorCodes | undefined> =
+    ErrorCode extends undefined
+      ? { status: 'SUCCESS'; data?: unknown; message?: string }
+      : {
+          status: 'ERROR'
+          message?: (typeof ErrorCodes)[ErrorCode]
+          error: ErrorCode
+          data?: { issues: ZodIssue[] } | { details: string }
+        }
+
+  type RequestHandler<
+    P = core.ParamsDictionary,
+    ResBody = ResponseBody<undefined | keyof typeof ErrorCodes>,
+    ReqBody = unknown,
+    ReqQuery = core.Query,
+    Locals extends Record<string, unknown> = Record<string, unknown>,
+  > = ExpressRequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>
+
   namespace Express {
     interface Request {
-      user?: JwtPayload | string
+      accessToken?: CustomJwtPayload | JwtPayload
     }
   }
 
   namespace NodeJS {
     interface ProcessEnv {
-      ADMINER_PORT: string | number
+      ADMINER_PORT: number | string
       DATABASE_NAME: string
-      DATABASE_PORT: string | number
+      DATABASE_PORT: number | string
       DATABASE_URL: string
       DOMAIN: string
-      NODE_ENV: 'development' | 'production'
+      NODE_ENV: 'development' | 'production' | 'test'
       NODE_MAILER_PASS: string
       NODE_MAILER_SENDER: string
       NODE_MAILER_SERVICE: string
       NODE_MAILER_USER: string
       PEPPER_SECRET: string
-      PORT: string | number
+      PORT: number | string
       PROJECT_NAME: string
-      TOKEN_SECRET: string
+      ACCESS_TOKEN_SECRET: string
+      ACCESS_TOKEN_MAX_AGE: number | string
+      REFRESH_TOKEN_SECRET: string
+      REFRESH_TOKEN_MAX_AGE: number | string
     }
   }
-
-  type RequestBody<ErrorCode extends keyof typeof ErrorCodes | undefined> =
-    ErrorCode extends undefined
-      ? { status: 'SUCCESS'; data?: unknown; message?: string }
-      : {
-          status: 'ERROR'
-          data?: { issues: ZodIssue[] }
-          error: ErrorCode
-          message?: (typeof ErrorCodes)[ErrorCode]
-        }
-
-  type RequestHandler<
-    P = core.ParamsDictionary,
-    ResBody = RequestBody<undefined | keyof typeof ErrorCodes>,
-    ReqBody = unknown,
-    ReqQuery = core.Query,
-    Locals extends Record<string, unknown> = Record<string, unknown>,
-  > = ExpressRequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>
 }
