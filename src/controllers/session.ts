5e8d97f1cd046a275cf8a15dafbd0538b3c99f3a
Gustavo Minetoma <gminetoma@gmail.com>
2024-12-31 14:46:12 +0900

    refactor: extract session token generation routine
    - Add database refresh token verification.

--- src/controllers/session.ts
+++ src/controllers/session.ts
@@ -5,6 +5,54 @@ import { generateSessionToken, hashString, verifyHashedString } from '../utils'
 import { ErrorCodes } from '../types'
 import jwt from 'jsonwebtoken'
 import { Routes } from '../routes'
+import { Response } from 'express'
+
+/**
+ * This function will mutate the response object
+ */
+const generateSessionTokens = async (
+  response: Response,
+  account: RequestAccount,
+) => {
+  const accessToken = generateSessionToken(account, 'access')
+  const refreshToken = generateSessionToken(account, 'refresh')
+
+  const hashedToken = await hashString(refreshToken)
+
+  await prisma.refreshToken.upsert({
+    create: {
+      accountId: account.id,
+      expiresAt: new Date(
+        Date.now() + Number(process.env.REFRESH_TOKEN_MAX_AGE),
+      ),
+      tokenHash: hashedToken,
+    },
+    update: {
+      expiresAt: new Date(
+        Date.now() + Number(process.env.REFRESH_TOKEN_MAX_AGE),
+      ),
+      tokenHash: hashedToken,
+    },
+    where: {
+      accountId: account.id,
+    },
+  })
+
+  response.cookie('accessToken', accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: Number(process.env.ACCESS_TOKEN_MAX_AGE),
+  })
+
+  response.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: Number(process.env.REFRESH_TOKEN_MAX_AGE),
+    path: Routes.refreshMySession,
+  })
+}
 
 export const refresh: RequestHandler = async ({ cookies }, response) => {
   const refreshToken: string = cookies['refreshToken']
@@ -56,52 +104,42 @@ export const refresh: RequestHandler = async ({ cookies }, response) => {
 
       if (typeof payload === 'object') {
         const customPayload = payload as CustomJwtPayload
+
+        const refreshTokenFound = await prisma.refreshToken.findUnique({
+          where: {
+            accountId: customPayload.account.id,
+          },
+        })
+
+        if (!refreshTokenFound) {
+          response.status(401).json({
+            status: 'ERROR',
+            message: ErrorCodes['AUTH-007'],
+            error: 'AUTH-007',
+          })
+          return
+        }
+
+        const isMatch = verifyHashedString(
+          refreshTokenFound.tokenHash,
+          refreshToken,
+        )
+
+        if (!isMatch) {
+          response.status(401).json({
+            status: 'ERROR',
+            message: ErrorCodes['AUTH-007'],
+            error: 'AUTH-007',
+          })
+          return
+        }
+
         const account = customPayload.account
 
-        const newAccessToken = generateSessionToken(account, 'access')
-        const newRefreshToken = generateSessionToken(account, 'refresh')
-
-        const tokenHash = await hashString(newRefreshToken)
-
-        await prisma.refreshToken.upsert({
-          create: {
-            accountId: account.id,
-            expiresAt: new Date(
-              Date.now() + Number(process.env.REFRESH_TOKEN_MAX_AGE),
-            ),
-            tokenHash,
-          },
-          update: {
-            expiresAt: new Date(
-              Date.now() + Number(process.env.REFRESH_TOKEN_MAX_AGE),
-            ),
-            tokenHash,
-          },
-          where: {
-            accountId: account.id,
-          },
-        })
-
-        response.cookie('accessToken', newAccessToken, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
-          maxAge: Number(process.env.ACCESS_TOKEN_MAX_AGE),
-        })
-
-        response.cookie('refreshToken', newRefreshToken, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
-          maxAge: Number(process.env.REFRESH_TOKEN_MAX_AGE),
-          path: Routes.refreshMySession,
-        })
+        await generateSessionTokens(response, account)
 
         response.json({
           status: 'SUCCESS',
-          data: {
-            accessToken: newAccessToken,
-          },
         })
       }
     },
@@ -175,49 +213,9 @@ export const post: RequestHandler = async ({ body }, response) => {
     return
   }
 
-  const accessToken = generateSessionToken(account, 'access')
-  const refreshToken = generateSessionToken(account, 'refresh')
-
-  const hashedToken = await hashString(refreshToken)
-
-  await prisma.refreshToken.upsert({
-    create: {
-      accountId: account.id,
-      expiresAt: new Date(
-        Date.now() + Number(process.env.REFRESH_TOKEN_MAX_AGE),
-      ),
-      tokenHash: hashedToken,
-    },
-    update: {
-      expiresAt: new Date(
-        Date.now() + Number(process.env.REFRESH_TOKEN_MAX_AGE),
-      ),
-      tokenHash: hashedToken,
-    },
-    where: {
-      accountId: account.id,
-    },
-  })
-
-  response.cookie('accessToken', accessToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: Number(process.env.ACCESS_TOKEN_MAX_AGE),
-  })
-
-  response.cookie('refreshToken', refreshToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: Number(process.env.REFRESH_TOKEN_MAX_AGE),
-    path: Routes.refreshMySession,
-  })
+  await generateSessionTokens(response, account)
 
   response.json({
     status: 'SUCCESS',
-    data: {
-      account,
-    },
   })
 }
